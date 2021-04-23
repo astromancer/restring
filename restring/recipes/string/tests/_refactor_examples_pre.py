4cc63b9a5b5cb1cca46c041ccd4d38fd011fd6a4
astromancer <hannes@saao.ac.za>
2021-04-23 20:26:07 +0200

    add tests

--- restring/recipes/string/tests/_refactor_examples_pre.py
+++ restring/recipes/string/tests/_refactor_examples_pre.py
@@ -1,10 +1,30 @@
 
+# pylint: disable-all
 
-class _Test1
-    def _arithmetic(self, other, op):
-        #
-        if isinstance(other, TimeSeries):
-            # Can only really do time series if they are simultaneous
-            if self.n != other.n:
-                raise ValueError('Arithmetic on %s objects with different '
-                                 'sizes not permitted' % self.__class__)
+if isinstance(other, TimeSeries):
+    # Can only really do time series if they are simultaneous
+    if self.n != other.n:
+        raise ValueError('Arithmetic on %s objects with different '
+                         'sizes not permitted' % self.__class__)
+
+
+if n > N_MAX_TS_SAFETY:
+    raise TooManyToPlot(
+        'Received %i time series to plot. This is probably not what you wanted. Refusing since safety limit '
+        'is currently set to %i to avoid accidental compute intensive '
+        'commands from overwhelming system resources.'
+        % (n, N_MAX_TS_SAFETY))
+
+
+if (colours is not None) and (len(colours) < n):
+    logger.warning('Colour sequence has too few colours (%i < %i). Colours '
+                   'will repeat' % (len(colours), n))
+
+n = len(data)
+if 4 < n < 1:
+    raise ValueError('Invalid number of arguments: %i' % n)
+
+
+if len(times) != len(signals):
+    raise ValueError('Number of time and signal vectors do not correspond. Please provide explicit time stamps for all signal vectors when plotting ragged time series.'
+                        )
